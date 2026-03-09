4b1e0ed794fbbc97a6f115188fb95f9fd4aef2fd
Serhii Skurtul <serhii.skurtul@belightsoft.com>
2026-03-09 21:53:54 +0200

    remove unnecessary debug and release build artifacts

--- obj/Debug/net9.0/TCP-UDP demo.AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Reflection;
-[assembly: System.Reflection.AssemblyCompanyAttribute("TCP-UDP demo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("TCP-UDP demo")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TCP-UDP demo")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
